0aaa3c6041b13d61ef3632665e89956a3e1f9170
danpeacock <danpeacock96@gmail.com>
2022-08-24 15:03:55 +0100

    add vars

--- main.tf
+++ main.tf
@@ -32,11 +32,3 @@ resource "vault_aws_secret_backend_role" "admin" {
 }
 EOF
 }
-
-output "backend" {
-  value = vault_aws_secret_backend.aws.path
-}
-
-output "role" {
-  value = vault_aws_secret_backend_role.admin.name
-}
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "vault_address" {
+    type = password
+}
+
+variable "vault_address" {
+    type = string
+}
+
+variable "aws_access_key" {
+    type = string
+}
+
+variable "aws_secret_key" {
+  type = password
+}
