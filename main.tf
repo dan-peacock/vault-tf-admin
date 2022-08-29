81b317faf34143c8f55bc1203d370ba5c414490a
danpeacock <danpeacock96@gmail.com>
2022-08-30 00:19:03 +0100

    rg fix

--- main.tf
+++ main.tf
@@ -50,6 +50,6 @@ resource "vault_azure_secret_backend_role" "generated_role" {
 
   azure_roles {
     role_name = "Owner"
-    scope =  "/subscriptions/${var.azure_subscription_id}/resourceGroups/*"
+    scope =  "/subscriptions/${var.azure_subscription_id}/resourceGroups/"
   }
 }
