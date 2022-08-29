38a8caadb586a80f8ff745fb6cf00df80c1a7169
danpeacock <danpeacock96@gmail.com>
2022-08-29 23:29:14 +0100

    add azure auth

--- variables.tf
+++ variables.tf
@@ -17,3 +17,19 @@ variable "aws_secret_key" {
 variable "name" {
   type = string
 }
+
+variable "azure_client_id" {
+  type = string
+}
+
+variable "azure_client_secret" {
+  type = string
+}
+
+variable "azure_subscription_id" {
+  type = string
+}
+
+variable "azure_tenant_id" {
+  type = string
+}
